8039cfe42c9a27f6798cc6a3990a8e334d6266d3
Romain KOENIG <romain@kromatic.fr>
2023-08-02 16:17:58 +0200

    Bubble sort starts to look cool, let's improve it tomorrow (tests are passing)

--- source/BubbleSort/BubbleSort.go
+++ source/BubbleSort/BubbleSort.go
@@ -1,47 +1,142 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
 	"strconv"
 	"strings"
 )
 
-func main() {
-	// Prompt the user for a series of numbers.
-	fmt.Println("Enter a series of integers, separated by spaces:")
+// Dear reader, please note that I tried to make this code as simple as possible.
+// Functions are small and do one thing.
+// I hope you find it easy to read and understand.
 
-	// Use a scanner to read the line of input.
-	var input string
-	_, err := fmt.Scanln(&input)
+func main() {
+
+	numbers, err := GetNumbersToSort()
 	if err != nil {
-		fmt.Println("Error reading input: ", err)
+		log.Fatal(err)
 		return
 	}
 
-	// Split the input into a slice of strings on each space character.
-	numStrings := strings.Split(input, " ")
-
-	// Convert the slice of strings to a slice of integers.
-	numbers := make([]int, len(numStrings))
-	for i, numStr := range numStrings {
-		numbers[i], err = strconv.Atoi(numStr)
-		if err != nil {
-			fmt.Println("Error reading numbers: ", err)
-			return
-		}
-	}
-
 	// Sort the numbers.
-	sorted := BubbleSort(numbers)
+	BubbleSort(numbers)
 
 	// Print out the sorted numbers.
+	PrintResult(numbers)
+}
+
+func PrintResult(numbers []int) {
 	fmt.Println("Sorted numbers:")
-	for _, num := range sorted {
+	for _, num := range numbers {
 		fmt.Printf("%d ", num)
 	}
 	fmt.Println()
 }
 
-func BubbleSort(numbers []int) []int {
-	return []int{1, 2, 3} // a stub just to make the test pass
+func GetNumbersToSort() (numbers []int, err error) {
+
+	// Prompt the user for a series of numbers.
+	// Split the input into a slice of strings on each space character.
+	// Trim trailing newline
+	// if the user entered more than 10 numbers, exit
+	// Convert the slice of strings to a slice of integers.
+
+	numStrings, err := ManageUserInput()
+	if err != nil {
+		return nil, err
+	}
+
+	numbers, err = StringsToIntegers(numStrings)
+	if err != nil {
+		return nil, err
+	}
+	return
+}
+
+func StringsToIntegers(numStrings []string) (numbers []int, err error) {
+	// Create a slice of the appropriate size.
+	numbers = make([]int, len(numStrings))
+
+	for i, numStr := range numStrings {
+		numbers[i], err = strconv.Atoi(numStr)
+		if err != nil {
+			fmt.Println("Error reading numbers: ", err)
+			return nil, err
+		}
+	}
+	return numbers, nil
+}
+
+func ManageUserInput() (values []string, err error) {
+	fmt.Println("Enter a series of integers (maximum 10), separated by spaces:")
+
+	values = make([]string, 0, 10)
+
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Error reading input: ", err)
+		return
+	}
+
+	input = strings.TrimSpace(input)
+	values = strings.Split(input, " ")
+
+	if len(values) > 10 {
+		fmt.Println("Please enter 10 or fewer numbers.")
+		err = fmt.Errorf("Too many numbers")
+		return
+	}
+	return
+}
+
+// BubbleSort sorts a slice of integers using the bubble sort algorithm.
+
+// Here is how a bubble sort works:
+// 1. Start at the beginning of the list.
+// 2. Compare the first two elements.
+// 3. If the first is greater than the second, swap them.
+// 4. Go to the next pair, and so on, continuously making sweeps of the list until sorted.
+// 5. In doing so, the smaller items slowly "bubble" up to the beginning of the list.
+// 6. If we ever make a sweep without swapping, the list is sorted and we can stop.
+
+// We do it N-1 times, where N is the number of items in the list, to guarantee that it is sorted.
+// If we make a sweep and make no swaps, the list is sorted and we can stop
+
+func BubbleSort(numbers []int) {
+
+	for i := 0; i < len(numbers); i++ {
+
+		// At the beginning of each sweep, we setup a flag to indicate if we made a swap (we did not yet, so false)
+		didSwap := false
+		firstIndex := 0
+
+		// We need to make sure that we don't go out of bounds
+		for firstIndex < (len(numbers) - i - 1) {
+			firstNumber := numbers[firstIndex]
+			secondNumber := numbers[firstIndex+1]
+
+			// If the first number is greater than the second number, swap them
+			if firstNumber > secondNumber {
+				Swap(numbers, firstIndex)
+				// We made a swap, so set the flag to true
+				didSwap = true
+			}
+			firstIndex++
+		}
+		// if we didn't make a swap, the list is sorted and we can stop
+		if !didSwap {
+			break
+		}
+	}
+}
+
+// Swap swaps the position of two elements in a slice of integers.
+func Swap(numbers []int, index int) {
+	originalValueAtIndex := numbers[index]
+	numbers[index] = numbers[index+1]
+	numbers[index+1] = originalValueAtIndex
 }
--- source/BubbleSort/BubbleSort_test.go
+++ source/BubbleSort/BubbleSort_test.go
@@ -6,12 +6,107 @@ import (
 )
 
 func TestSortUserInput(t *testing.T) {
-	given := []int{3, 2, 1}
-	want := []int{1, 2, 3}
 
-	got := BubbleSort(given)
+	testCases := []struct {
+		name  string
+		given []int
+		want  []int
+	}{
+		{
+			name:  "Multiple numbers in reverse order",
+			given: []int{5, 4, 3, 2, 1},
+			want:  []int{1, 2, 3, 4, 5},
+		},
+		{
+			name:  "Empty slice",
+			given: []int{},
+			want:  []int{},
+		},
+		{
+			name:  "Single value",
+			given: []int{1},
+			want:  []int{1},
+		},
+		{
+			name:  "Negative numbers",
+			given: []int{-5, -1, -3, -2, -4},
+			want:  []int{-5, -4, -3, -2, -1},
+		},
+		{
+			name:  "Negative and positive numbers",
+			given: []int{-5, -1, 3, -2, 4},
+			want:  []int{-5, -2, -1, 3, 4},
+		},
+		{
+			name:  "Already sorted",
+			given: []int{1, 2, 3, 4, 5},
+			want:  []int{1, 2, 3, 4, 5},
+		},
+	}
 
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("BubbleSort(%v) = %v; want %v", given, got, want)
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			original := make([]int, len(tc.given))
+			copy(original, tc.given)
+
+			BubbleSort(tc.given)
+			if !reflect.DeepEqual(tc.given, tc.want) {
+				t.Errorf("BubbleSort(%v) = %v; want %v", original, tc.given, tc.want)
+			} else {
+				t.Logf("BubbleSort(%v) = %v; want %v - is OK", original, tc.given, tc.want)
+			}
+		})
+	}
+}
+
+func TestStringsToIntegers(t *testing.T) {
+	// Normal Cases
+	normalTestCases := []struct {
+		name  string
+		given []string
+		want  []int
+	}{
+		{
+			name:  "positive integers",
+			given: []string{"1", "2", "3"},
+			want:  []int{1, 2, 3},
+		},
+		{
+			name:  "negative integers",
+			given: []string{"-1", "-2", "-3"},
+			want:  []int{-1, -2, -3},
+		},
+	}
+
+	for _, tc := range normalTestCases {
+		t.Run(tc.name, func(t *testing.T) {
+			got, err := StringsToIntegers(tc.given)
+			if err != nil {
+				t.Fatalf("StringsToIntegers(%v) returned unexpected error: %v", tc.given, err)
+			}
+			if !reflect.DeepEqual(got, tc.want) {
+				t.Errorf("StringsToIntegers(%v) = %v; want %v", tc.given, got, tc.want)
+			}
+		})
+	}
+
+	// Error Cases
+	errorTestCases := []struct {
+		name  string
+		given []string
+	}{
+		{
+			name:  "non-integer strings",
+			given: []string{"a", "b", "c"},
+		},
+	}
+
+	for _, tc := range errorTestCases {
+		t.Run(tc.name, func(t *testing.T) {
+			_, err := StringsToIntegers(tc.given)
+			if err == nil {
+				t.Errorf("StringsToIntegers(%v) expected an error but got nil", tc.given)
+			}
+		})
 	}
 }
