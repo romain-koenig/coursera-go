2639694f40874026ebe7634d4a9b07ce1f8935d0
Romain KOENIG <romain@kromatic.fr>
2023-08-04 12:41:55 +0200

    Slowy working my way to the solution. Commit does not test OK

--- source/AnimalInformations/AnimalInformations.go
+++ source/AnimalInformations/AnimalInformations.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 )
 
 // We have a JSON data structure that holds information about animals.
-var AnimalInformations = []byte(`{
-	"animals": [
+const data = `
+	[
 	  {
 		"name": "cow",
 		"info": {
@@ -32,8 +34,7 @@ var AnimalInformations = []byte(`{
 		}
 	  }
 	]
-  }
-`)
+`
 
 // We create an Animal "class" to hold the information about a single animal.
 type Animal struct {
@@ -41,6 +42,7 @@ type Animal struct {
 	Info map[string]string
 }
 
+// The GetAnimalInformations function takes a string as input (from the user) and returns a string as output (which will be printed on screen).
 func GetAnimalInformations(request string) string {
 	return "grass"
 }
@@ -49,5 +51,27 @@ func main() {
 	fmt.Println("Animal Informations")
 	fmt.Println("-------------------")
 	fmt.Println("")
+
+	// Let's create an Animal array and unmarshal the JSON data into it.
+
+	var animals []Animal
+
+	err := json.Unmarshal([]byte(data), &animals)
+
+	if err != nil {
+		// handle error
+		log.Fatal(err)
+	}
+
+	for _, animal := range animals {
+		fmt.Println("Name:", animal.Name)
+		for key, value := range animal.Info {
+			fmt.Println(key, ":", value)
+		}
+		fmt.Println("")
+	}
+
+	// Let's ask the user for the name of the animal and the information they want to know about it.
+	// We'll then call GetAnimalInformations to get the information and print it on screen.
 
 }
