6074bcdfbd8c9a41fc2a0a4b0629d6168c3e8dbe
Romain KOENIG <romain@kromatic.fr>
2023-08-04 14:35:18 +0200

    Version that will be provided for review

--- source/AnimalInformations/AnimalInformations.go
+++ source/AnimalInformations/AnimalInformations.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"log"
+	"os"
+	"strings"
 )
 
-// We have a JSON data structure that holds information about animals.
+// We have a JSON data structure that holds information about animals. (was a table in the exercise)
+// I took the liberty to add a kangaroo to the list.
 const data = `
 	[
 	  {
@@ -33,6 +37,15 @@ const data = `
 		  "Spoken sound": "hsss"
 		}
 	  }
+	  ,
+	  {
+		"name": "kangaroo",
+		"info": {
+		  "Food eaten": "grass",
+		  "Locomotion method": "jump",
+		  "Spoken sound": "boing"
+		}
+	  }
 	]
 `
 
@@ -42,11 +55,53 @@ type Animal struct {
 	Info map[string]string
 }
 
-// The GetAnimalInformations function takes a string as input (from the user) and returns a string as output (which will be printed on screen).
-func GetAnimalInformations(request string) string {
-	return "grass"
+// We then define 3 methods, that will be used to get the information about the animal. These methode use the "class" Animal as a receiver type.
+
+// Eat method will return the food the animal eats.
+func (a Animal) Eat() string {
+	return a.Info["Food eaten"]
 }
 
+// Move method will return the locomotion method of the animal.
+func (a Animal) Move() string {
+	return a.Info["Locomotion method"]
+}
+
+// Speak method will return the sound the animal makes.
+func (a Animal) Speak() string {
+	return a.Info["Spoken sound"]
+}
+
+// GetAnimalInformations function will be used to get the information about the animal the user is looking for. We give it the list of animals, the name of the animal and the information the user wants to know about it.
+func GetAnimalInformations(animals []Animal, animalName string, information string) string {
+
+	// We loop through the animals array to find the animal the user is looking for.
+
+	for _, animal := range animals {
+		if animal.Name == animalName {
+			// We found the animal the user is looking for.
+			// Let's check what information the user wants to know about the animal.
+
+			switch information {
+			case "eat":
+				return animal.Eat()
+			case "move":
+				return animal.Move()
+			case "speak":
+				return animal.Speak()
+			default:
+				return "Unknown information request"
+
+			}
+		}
+	}
+
+	return "Animal not found."
+
+	// Note: as we sanitized the user input, error cases should not happen here. We still check for them in case the code is modified later, or called from another function.
+}
+
+// Here is where the program starts.
 func main() {
 	fmt.Println("Animal Informations")
 	fmt.Println("-------------------")
@@ -63,15 +118,77 @@ func main() {
 		log.Fatal(err)
 	}
 
-	for _, animal := range animals {
-		fmt.Println("Name:", animal.Name)
-		for key, value := range animal.Info {
-			fmt.Println(key, ":", value)
-		}
-		fmt.Println("")
-	}
-
 	// Let's ask the user for the name of the animal and the information they want to know about it.
 	// We'll then call GetAnimalInformations to get the information and print it on screen.
 
+	command, err := ManageUserInput(animals)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+
+	println(GetAnimalInformations(animals, command[0], command[1]))
+
+}
+
+// contains is used to check if a string is in a string array. It's just a helper function.
+// Go version 1.21 should make it obsolete, please see https://tip.golang.org/doc/go1.21#slices
+func contains(s []string, str string) bool {
+	for _, v := range s {
+		if v == str {
+			return true
+		}
+	}
+
+	return false
+}
+
+// ManageUserInput is used to get the user input and sanitize it.
+func ManageUserInput(animals []Animal) (values []string, err error) {
+
+	availableAnimals := make([]string, 0, len(animals))
+	// Let's parse the animals and see which animals are available.
+	for _, animal := range animals {
+		// if the animal is not already in the availableAnimals array, we add it.
+		if !contains(availableAnimals, animal.Name) {
+			availableAnimals = append(availableAnimals, animal.Name)
+		}
+	}
+
+	fmt.Println("Enter a a command: an animal and an information request (eat, move or speak).")
+	fmt.Println("Available animals: ", availableAnimals)
+	fmt.Printf(`> `)
+
+	values = make([]string, 0, 2)
+
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Error reading input: ", err)
+		return nil, err
+	}
+
+	input = strings.TrimSpace(input)
+	values = strings.Split(input, " ")
+
+	if len(values) != 2 {
+		fmt.Println("Please enter 2 words: an animal (cow, bird or snake) and an information request (eat, move or speak).")
+		err = fmt.Errorf("Invalid request")
+		return nil, err
+	}
+
+	if !contains(availableAnimals, values[0]) {
+		fmt.Println("Please enter an animal.")
+		fmt.Println("Available animals: ", availableAnimals)
+		err = fmt.Errorf("Invalid animal")
+		return nil, err
+	}
+
+	if values[1] != "eat" && values[1] != "move" && values[1] != "speak" {
+		fmt.Println("Please enter an information request (eat, move or speak).")
+		err = fmt.Errorf("Invalid information request")
+		return nil, err
+	}
+
+	return
 }
--- source/AnimalInformations/AnimalInformations_test.go
+++ source/AnimalInformations/AnimalInformations_test.go
@@ -4,31 +4,186 @@ import (
 	"testing"
 )
 
+var constAnimals = []Animal{
+	{
+		Name: "cow",
+		Info: map[string]string{
+			"Food eaten":        "grass",
+			"Locomotion method": "walk",
+			"Spoken sound":      "moo",
+		},
+	},
+	{
+		Name: "bird",
+		Info: map[string]string{
+			"Food eaten":        "worms",
+			"Locomotion method": "fly",
+			"Spoken sound":      "peep",
+		},
+	},
+	{
+		Name: "snake",
+		Info: map[string]string{
+			"Food eaten":        "mice",
+			"Locomotion method": "slither",
+			"Spoken sound":      "hsss",
+		},
+	},
+}
+
 func TestGetAnimalInformations(t *testing.T) {
-	// Define test cases.
-	testCases := []struct {
-		name  string
-		given string
-		want  string
+	tests := []struct {
+		name         string
+		animals      []Animal
+		animalName   string
+		information  string
+		wantResponse string
 	}{
 		{
-			name:  "Example values for COW",
-			given: "cow eat",
-			want:  "grass",
+			name:         "Cow Eating Test",
+			animals:      constAnimals,
+			animalName:   "cow",
+			information:  "eat",
+			wantResponse: "grass",
 		},
 		{
-			name:  "Example values for BIRD",
-			given: "bird move",
-			want:  "fly",
+			name:         "Bird Moving Test",
+			animals:      constAnimals,
+			animalName:   "bird",
+			information:  "move",
+			wantResponse: "fly",
+		},
+
+		{
+			name:         "Snake Speaking Test",
+			animals:      constAnimals,
+			animalName:   "snake",
+			information:  "speak",
+			wantResponse: "hsss",
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			if gotResponse := GetAnimalInformations(tc.animals, tc.animalName, tc.information); gotResponse != tc.wantResponse {
+				t.Errorf("GetAnimalInformations(%v, %v, %v) = %v, want %v", tc.animals, tc.animalName, tc.information, gotResponse, tc.wantResponse)
+			}
+		})
+	}
+}
+
+func TestEat(t *testing.T) {
+	// Define test cases.
+	testCases := []struct {
+		name    string
+		animal  Animal
+		wantEat string
+	}{
+		{
+			name: "Cow Eating Test",
+			animal: Animal{
+				Name: "cow",
+				Info: map[string]string{
+					"Food eaten": "grass",
+				},
+			},
+			wantEat: "grass",
+		},
+		{
+			name: "Snake Eating Test",
+			animal: Animal{
+				Name: "snake",
+				Info: map[string]string{
+					"Food eaten": "mice",
+				},
+			},
+			wantEat: "mice",
 		},
 	}
 
 	// Run test cases.
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			got := GetAnimalInformations(tc.given)
-			if got != tc.want {
-				t.Errorf("GetAnimalInformations(%v) = %v; want %v", tc.given, got, tc.want)
+			if gotEat := tc.animal.Eat(); gotEat != tc.wantEat {
+				t.Errorf("Animal.Eat() = %v, want %v", gotEat, tc.wantEat)
+			}
+		})
+	}
+}
+
+func TestMove(t *testing.T) {
+	// Define test cases.
+	testCases := []struct {
+		name     string
+		animal   Animal
+		wantMove string
+	}{
+		{
+			name: "Cow Moving Test",
+			animal: Animal{
+				Name: "cow",
+				Info: map[string]string{
+					"Locomotion method": "walk",
+				},
+			},
+			wantMove: "walk",
+		},
+		{
+			name: "Snake Moving Test",
+			animal: Animal{
+				Name: "snake",
+				Info: map[string]string{
+					"Locomotion method": "slither",
+				},
+			},
+			wantMove: "slither",
+		},
+	}
+
+	// Run test cases.
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			if gotMove := tc.animal.Move(); gotMove != tc.wantMove {
+				t.Errorf("Animal.Move() = %v, want %v", gotMove, tc.wantMove)
+			}
+		})
+	}
+}
+
+func TestSpeak(t *testing.T) {
+	// Define test cases.
+	testCases := []struct {
+		name      string
+		animal    Animal
+		wantSpeak string
+	}{
+		{
+			name: "Cow Speaking Test",
+			animal: Animal{
+				Name: "cow",
+				Info: map[string]string{
+					"Spoken sound": "moo",
+				},
+			},
+			wantSpeak: "moo",
+		},
+		{
+			name: "Snake Speaking Test",
+			animal: Animal{
+				Name: "snake",
+				Info: map[string]string{
+					"Spoken sound": "hsss",
+				},
+			},
+			wantSpeak: "hsss",
+		},
+	}
+
+	// Run test cases.
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			if gotSpeak := tc.animal.Speak(); gotSpeak != tc.wantSpeak {
+				t.Errorf("Animal.Speak() = %v, want %v", gotSpeak, tc.wantSpeak)
 			}
 		})
 	}
