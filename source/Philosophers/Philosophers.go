df710f051baaba26e5babb3c78ead3d1b00785e9
Romain KOENIG <romain@kromatic.fr>
2023-08-11 12:11:09 +0200

    probably the last version. this one is pretty cool

--- source/Philosophers/Philosophers.go
+++ source/Philosophers/Philosophers.go
@@ -1,30 +1,31 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"math/rand"
 	"sync"
 	"time"
 )
 
-const numberOfPortions = 3
-const numberOfPhilosophers = 5
-
 type ChopS struct{ sync.Mutex }
 
 type Philo struct {
+	index           int
 	id              int
 	portionsLeft    int
 	leftCS, rightCS *ChopS
 }
 
+type Channels struct {
+	requestChannel        chan int
+	personalChannels      []chan bool
+	finishedEatingChannel chan int
+}
+
 // coinFlip returns true or false randomly. Simple helper function.
 func coinFlip() bool {
-	coinFlip := rand.Intn(2)
-	if coinFlip == 1 {
-		return true
-	}
-	return false
+	return rand.Intn(2) == 1
 }
 
 func howManyPhilosophersAreEating(philosophersIsEating []bool) int {
@@ -38,21 +39,33 @@ func howManyPhilosophersAreEating(philosophersIsEating []bool) int {
 }
 
 // Host is the goroutine that will make sure that only two philosophers are eating at the same time
-func Host(requestChannel <-chan int, personalChannels []chan bool, wg *sync.WaitGroup) {
+func Host(communicationChannels Channels,
+	numberOfPhilosophers int,
+	wg *sync.WaitGroup) {
 	defer wg.Done()
 
 	// philosophersIsEating is a slice that will keep track of which philosophers are eating.
 	philosophersIsEating := make([]bool, numberOfPhilosophers)
 	waitingQueue := []int{} // A queue to store waiting philosophers.
 
+	philosophersDone := 0
+
 	for {
 		select {
-		case philoID := <-requestChannel:
+
+		case finishedPhiloID := <-communicationChannels.finishedEatingChannel:
+			philosophersDone++
+			philosophersIsEating[finishedPhiloID] = false
+			if philosophersDone == numberOfPhilosophers {
+				return
+			}
+
+		case philoID := <-communicationChannels.requestChannel:
 			if !philosophersIsEating[philoID] {
 				if howManyPhilosophersAreEating(philosophersIsEating) < 2 {
 					// Grant permission
 					philosophersIsEating[philoID] = true
-					personalChannels[philoID] <- true
+					communicationChannels.personalChannels[philoID] <- true
 				} else {
 					// Add philosopher to waiting queue.
 					waitingQueue = append(waitingQueue, philoID)
@@ -68,27 +81,30 @@ func Host(requestChannel <-chan int, personalChannels []chan bool, wg *sync.Wait
 
 					// Grant permission.
 					philosophersIsEating[nextPhilo] = true
-					personalChannels[nextPhilo] <- true
+					communicationChannels.personalChannels[nextPhilo] <- true
 				}
 			}
 		case <-time.After(time.Second * 10):
 			// Some reasonable timeout
-			fmt.Println("No more requests, host is done.")
+			fmt.Println("Timeout, host is exiting.") // Note: this should never happen. It's only a failsafe.
 			return
 		}
 	}
 }
 
-func (p Philo) eat(requestChannel chan<- int, personalChannels []chan bool) {
+func (p Philo) eat(communicationChannels Channels,
+	wg *sync.WaitGroup) {
+
+	defer wg.Done()
 
 	for p.portionsLeft > 0 {
 
 		// Ask for permission to eat
-		requestChannel <- p.id
+		communicationChannels.requestChannel <- p.index
 
 		// Wait for permission
 
-		<-personalChannels[p.id]
+		<-communicationChannels.personalChannels[p.index]
 
 		p.pickCS()
 
@@ -102,8 +118,11 @@ func (p Philo) eat(requestChannel chan<- int, personalChannels []chan bool) {
 		p.releaseCS()
 
 		// Inform the host that you're done eating your portion
-		requestChannel <- p.id
+		communicationChannels.requestChannel <- p.index
 	}
+
+	// Once all portions are eaten
+	communicationChannels.finishedEatingChannel <- p.index
 }
 
 func (p Philo) releaseCS() {
@@ -122,46 +141,81 @@ func (p Philo) pickCS() {
 }
 
 func main() {
+
+	numberOfPhilosophers := flag.Int("n", 5, "Number of philosophers")
+	numberOfPortions := flag.Int("p", 3, "Number of portions per philosopher")
+	flag.Parse()
+
+	if *numberOfPhilosophers < 2 {
+		fmt.Println("There must be at least two philosophers.")
+		return
+	}
+
+	if *numberOfPhilosophers > 8000 {
+		fmt.Println("There can't be more than 8000 philosophers (hard limit on the number of goroutines).")
+		return
+	}
+
+	if *numberOfPortions < 1 {
+		fmt.Println("There must be at least one portion per philosopher.")
+		return
+	}
+
 	rand.Seed(time.Now().UnixNano())
-	fmt.Println("Hello, Philosophers!")
+	fmt.Println("Welcome to the dining philosophers problem!")
+	fmt.Println("-------------------------------------------")
+	fmt.Println()
+	fmt.Println("You can change the number of philosophers and the number of portions per philosopher using the -n and -p flags.")
+	fmt.Println("Example: >go run Philosophers.go -n 10 -p 5")
+	fmt.Println()
+	fmt.Println("Number of philosophers:", *numberOfPhilosophers)
+	fmt.Println("Number of portions per philosopher:", *numberOfPortions)
+	fmt.Println()
 
 	// Create a WaitGroup
 	var wg sync.WaitGroup
 
-	// Add 5 to the WaitGroup
-	wg.Add(1)
+	// Add all the philosophers + the host to the WaitGroup
+	wg.Add(*numberOfPhilosophers + 1)
 
 	// initialize the ChopSticks
-	CSticks := make([]*ChopS, numberOfPhilosophers)
-	for i := 0; i < 5; i++ {
+	CSticks := make([]*ChopS, *numberOfPhilosophers)
+	for i := 0; i < *numberOfPhilosophers; i++ {
 		CSticks[i] = new(ChopS)
 	}
 
 	// Initialize the Philosophers
-	philos := make([]*Philo, numberOfPhilosophers)
-	for i := 0; i < numberOfPhilosophers; i++ {
-		philos[i] = &Philo{i, numberOfPortions, CSticks[i], CSticks[(i+1)%numberOfPhilosophers]}
+	philos := make([]*Philo, *numberOfPhilosophers)
+	for i := 0; i < *numberOfPhilosophers; i++ {
+		philos[i] = &Philo{i, i + 1, *numberOfPortions, CSticks[i], CSticks[(i+1)%*numberOfPhilosophers]}
 	}
 
 	// Create the request channel
 	requestChannel := make(chan int)
 
 	// Create the personal channels
-	personalChannels := make([]chan bool, numberOfPhilosophers)
-	for i := 0; i < numberOfPhilosophers; i++ {
+	personalChannels := make([]chan bool, *numberOfPhilosophers)
+	for i := 0; i < *numberOfPhilosophers; i++ {
 		personalChannels[i] = make(chan bool)
 	}
 
+	// Add a channel for philosophers to notify the host they're done
+	finishedEatingChannel := make(chan int)
+
+	communicationChannels := Channels{requestChannel, personalChannels, finishedEatingChannel}
+
 	// Start the host
 
-	go Host(requestChannel, personalChannels, &wg)
+	go Host(communicationChannels, *numberOfPhilosophers, &wg)
 
 	// Make the philosophers eat
-	for i := 0; i < numberOfPhilosophers; i++ {
-		go philos[i].eat(requestChannel, personalChannels)
+	for i := 0; i < *numberOfPhilosophers; i++ {
+		go philos[i].eat(communicationChannels, &wg)
 	}
 
 	// Maker sure that the host has finished before exiting
 	wg.Wait()
+
+	fmt.Println("All philosophers are done eating, host has exited, program is done.")
 
 }
